ca3d280ad9dd96d067193552dfe8d7436fe370d9
Idan Stark <idanstark42@gmail.com>
2019-04-11 00:26:15 +0300

    removed space

--- test/mocks/ms-correlation.mock.js
+++ test/mocks/ms-correlation.mock.js
@@ -6,7 +6,6 @@ const spies = require('chai-spies')
 chai.use(spies)
 
 const correlationId = '123456'
-
 const getCorrelationId = chai.spy.returns(correlationId)
 
 exports.MSCorrelationMock = { getCorrelationId }
