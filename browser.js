86db7c0887b573c318bef2fb8c7ee6eb3a5ab538
Idan Stark <idanstark42@gmail.com>
2019-11-26 15:55:07 +0200

    new architecture reboot

--- browser.js
+++ browser.js
@@ -1,12 +1,9 @@
-const { MClient } = require('mhub/dist/src/nodeclient')
+const { MClient } = require('mhub/dist/src/browserclient')
 
 const { createMessenger } = require('./lib/messenger_factory')
-const { correlateMesseger } = require('./lib/correlation')
 
 exports.createMessenger = options => {
   const messenger = createMessenger(new MClient(options.mhubURI), options)
-
-  correlateMesseger(messenger)
 
   return messenger
 }
